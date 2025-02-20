4b3a749dbc774bc78adeee21c30f7a13ce3931c5
AnusriVarierWeb <anusrivarier@gmail.com>
2025-02-20 11:27:49 +0000

    initial commit

---
+++ EkaForestWebsite/js/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // You can add more functionality here, like sending the form data to a server.
+});
